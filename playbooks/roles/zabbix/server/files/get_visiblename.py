54108d562f3ae47c000befae2541bace1a10ec71
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2021-06-02 13:25:52 +0300

    ci: migrate roles/zabbix/server 'get_visiblename.py' script to use requests module

--- playbooks/roles/zabbix/server/files/get_visiblename.py
+++ playbooks/roles/zabbix/server/files/get_visiblename.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import urllib.request, urllib.error, urllib.parse
+import requests
 import json
 import sys
 import os
@@ -13,47 +14,53 @@ if not user or not password or not server:
     print("Environment vars must be set: ZABBIX_SERVER, ZABBIX_USER, ZABBIX_PASS")
     sys.exit(1)
 
-api = server + "/api_jsonrpc.php"
+api_url = server + "/api_jsonrpc.php"
 
-def get_token():
+def get_token(api_url):
     data = {'jsonrpc': '2.0', 'method': 'user.login', 'params':
             {'user': user, 'password': password}, 'id': '0'}
-    req = urllib.request.Request(api)
     data_json = json.dumps(data).encode()
-    req.add_header('content-type', 'application/json-rpc')
     try:
-        response = urllib.request.urlopen(req, data_json)
-    except urllib.error.HTTPError as ue:
-        print("Error token: " + str(ue))
+        response = requests.post(api_url, headers={'content-type': 'application/json-rpc; charset=utf8'}, data=data_json)
+    except requests.exceptions.HTTPError as err:
+        print("Error requesting token: {}".format(err))
         sys.exit(1)
-    else:
-        authtoken = str(response.read().decode('utf8').split(',')[1].split(':')[1].strip('"'))
-        return authtoken
+    except requests.exceptions.ConnectionError as err:
+        print("Error requesting host.get: {}".format(err))
+        sys.exit(1)
 
-def get_host_visual_names(host_pattern):
+    return response.json().get('result', None)
+
+def get_host_visual_names(api_url, host_pattern):
     data = {"jsonrpc": "2.0", "method": "host.get", "params":
             {"output": "extend", "search": {"host": [host_pattern]}},
             "startSearch": 1, "auth": token, "id": 0}
-    req = urllib.request.Request(api)
     data_json = json.dumps(data).encode()
-    req.add_header('content-type', 'application/json-rpc')
     try:
-        response = urllib.request.urlopen(req, data_json)
-    except urllib.error.HTTPError as ue:
-        print("Error host.get: " + str(ue))
-    else:
-        try:
-            res = json.loads(response.read())
-        except:
-            res = {}
-        hosts = [host.get('name', '') for host in res.get('result', [])]
-        return hosts
+        response = requests.post(api_url, headers={'content-type': 'application/json-rpc; charset=utf8'}, data=data_json)
+    except requests.exceptions.HTTPError as err:
+        print("Error requesting host.get: {}".format(err))
+        sys.exit(1)
+    except requests.exceptions.ConnectionError as err:
+        print("Error requesting host.get: {}".format(err))
+        sys.exit(1)
+
+    hosts = []
+    try:
+        res = json.loads(response.text)
+    except:
+        res = {}
+
+    for host in res.get('result', []):
+        hosts.append(host.get('name', ''))
+
+    return hosts
 
 if len(sys.argv) == 2:
-    token = get_token()
-    host_names = get_host_visual_names(sys.argv[1])
+    token = get_token(api_url)
+    host_names = get_host_visual_names(api_url, sys.argv[1])
     if host_names:
-        print("\n".join(host_names).encode('utf-8').strip())
+        print(u"\n".join(host_names).strip())
 else:
     print("Usage: ZABBIX_SERVER=https://server ZABBIX_USER=user ZABBIX_PASS=pass " \
           "{} <host_pattern>".format(sys.argv[0]))
